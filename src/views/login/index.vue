ee29a46f34bb3f08f3c318be0311f1166fd0f3ee
John <xzq664673913.163.com>
2024-12-22 12:24:23 +0800

    ss

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -18,7 +18,7 @@
       <div class="w-320 flex-col px-20 py-32">
         <h2 class="f-c-c text-24 text-#6a6a6a font-normal">
           <img src="@/assets/images/logo.png" class="mr-12 h-50">
-          {{ title }}
+          {{ title }}111
         </h2>
         <n-input
           v-model:value="loginInfo.username"
